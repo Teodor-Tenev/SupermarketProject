937c97eb8e62711cde3085a4965a725128f3cd6a
phoenixcat <teodor.tenev@abv.bg>
2013-01-04 20:58:00 +0200

    first commit

---
+++ Supermarket/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Supermarket.DAL;
+
+namespace Supermarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult ViewAllProducts()
+        {
+            List<Product> list = ProductDAL.GetAllProducts();
+            return View(list);
+        }
+    }
+}
