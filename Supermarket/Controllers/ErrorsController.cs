5c828c700100076838a708ea3edc8905fb215750
phoenixcat <teodor.tenev@abv.bg>
2013-02-13 16:10:22 +0200

    try to fix the DB

--- Supermarket/Controllers/ErrorsController.cs
+++ Supermarket/Controllers/ErrorsController.cs
@@ -8,16 +8,29 @@ namespace Supermarket.Controllers
 {
     public class ErrorsController : Controller
     {
+        /// <summary>
+        /// General exception
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
         public ActionResult General(Exception exception)
         {
             return Content("General failure", "text/plain");
         }
 
+        /// <summary>
+        /// Action for HTTP404 exception
+        /// </summary>
+        /// <returns>Page not found</returns>
         public ActionResult Http404()
         {
             return View("PageNotFound");
         }
 
+        /// <summary>
+        /// Action for HTTP403 exception
+        /// </summary>
+        /// <returns>Page not allowed</returns>
         public ActionResult Http403()
         {
             return View("PageNotAllowed");
